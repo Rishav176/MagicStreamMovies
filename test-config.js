ae84a97cc9d20f2afc7a849c93d849c3e0fd4cb5
Rishav176 <rishavkr1766@gmail.com>
2025-11-13 00:27:35 -0700

    Add test config file

---
+++ test-config.js
@@ -0,0 +1,7 @@
+const config = {
+  apiKey: "AKIAIOSFODNN7EXAMPLE",
+  githubToken: "ghp_1234567890abcdefghijklmnopqrstuv12345",
+  openaiKey: "sk-1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJ"
+};
+
+module.exports = config;
